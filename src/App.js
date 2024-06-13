0413a33e3d45cc72f3b8e2d7ed33759b495db231
Yasmixe <yasminehanafi59@gmail.com>
2024-06-13 12:53:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Parallax, ParallaxLayer} from '@react-spring/parallax'
+import TextBlock from './TextBlock';
+import './App.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Parallax pages={2} style={{ top: '0', left: '0' }} className='animation'>
+      <ParallaxLayer offset={0} speed={0.25}>
+        <div className='animation_layer parallax' id='artback'>
+
+        </div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={0.4}>
+        <div className='animation_layer parallax' id='mountain        '>
+
+        </div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={-0.1}>
+        <div className='animation_layer parallax' id='logoland'>
+
+        </div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={-0.3}>
+        <div className='animation_layer parallax' id='jungle1'>
+
+        </div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={-0.35}>
+        <div className='animation_layer' id='jungle2'>
+
+        </div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={-0.5}>
+        <div className='animation_layer parallax' id='jungle3'>
+
+        </div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={-0.45}>
+        <div className='animation_layer parallax' id='jungle4'>
+
+        </div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={-0.4}>
+        <div className='animation_layer parallax' id='manonmountain'>
+
+        </div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={0.35}>
+        <div className='animation_layer parallax' id='jungle5'>
+
+        </div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={1} speed={0.25}>
+          <TextBlock />
+        </ParallaxLayer>
+      
+    </Parallax>
   );
 }
 
